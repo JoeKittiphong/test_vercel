80d2b6ce68c087cd98b8f74454f7c9248015fe8d
Zylaah <joe_star@outlook.com>
2023-07-16 20:43:52 +0700

    add cors

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 
 const connectMonogDB = require("./mongoDB/connection")
 const blog = require("./mongoDB/model")
 
 
-app.listen(3000,async ()=>{
+app.listen(3000, async () => {
     await connectMonogDB()
 })
 
 app.use(express.json())
-app.use(express.urlencoded({extended : false}))
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
-app.get("/",()=>{
+app.get("/", () => {
     res.send("api is running")
 })
 
-app.post("/create", async(req, res)=>{
-    await blog.create(req.body)
-    res.send("created")
+app.post("/create", async (req, res) => {
+    try {
+        const { title, cover, content } = req.body
+        if (!title || !cover || !content) {
+            res.send("can not create")
+        } else {
+            await blog.create({ title, cover, content })
+            res.send("created")
+        }
+    } catch (error) {
+        res.send(error)
+    }
 })
 
-app.get("/read", async(req, res)=>{
+app.get("/read", async (req, res) => {
     const db = await blog.find()
     res.send(JSON.stringify(db))
 })
 
-app.get("/readId/:id", async(req, res)=>{
+app.get("/readId/:id", async (req, res) => {
     const db = await blog.findById(req.params.id)
     res.send(JSON.stringify(db))
 })
 
-app.put("/update/:id", async(req, res)=>{
-    await blog.findByIdAndUpdate(req.params.id,req.body)
+app.put("/update/:id", async (req, res) => {
+    await blog.findByIdAndUpdate(req.params.id, req.body)
     res.send(`${req.params.id} is updated`)
 })
 
-app.delete("/delete/:id", async(req, res)=>{
+app.delete("/delete/:id", async (req, res) => {
     await blog.findByIdAndDelete(req.params.id)
     res.send(`${req.params.id} is deleted`)
 })
